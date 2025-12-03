2fb0d5cd09e35f9f98ed8aeab98893bb43b735d7
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-12-04 00:48:49 +0200

    video elements

--- index.js
+++ index.js
@@ -3,27 +3,36 @@
 
     let tl = gsap.timeline();
     
-    tl.to('.slide-0', {xPercent: -100})
-    tl.from('.slide-1', {xPercent: 100}, "<")
-    tl.from('.slide-1 .bar', {width: 0, height: 0, opacity: 0 }, "<");
+    tl
+    .addLabel('slide-0')
+    .to('.slide-0', {xPercent: -100})
+    .from('.slide-1', {xPercent: 100}, "<")
+    .from('.slide-1 .video', {scale: 0.8, opacity: 0 }, "<")
     
-    tl.to('.slide-1', {xPercent: -100})
-    tl.from('.slide-2', {xPercent: 100}, "<")
+    .addLabel('slide-1')
+    .to('.slide-1', {xPercent: -100})
+    .to('.slide-1 .video', {scale: 0.8, opacity: 0 }, "<")
+    .from('.slide-2', {xPercent: 100}, "<")
 
-    tl.to('.slide-2', {xPercent: -100})
-    tl.from('.slide-3', {xPercent: 100}, "<")
+    .addLabel('slide-2')
+    .to('.slide-2', {xPercent: -100})
+    .from('.slide-3', {xPercent: 100}, "<")
 
-    tl.to('.slide-3', {xPercent: -100})
-    tl.from('.slide-4', {xPercent: 100}, "<")
+    .addLabel('slide-3')
+    .to('.slide-3', {xPercent: -100})
+    .from('.slide-4', {xPercent: 100}, "<")
 
-    tl.to('.slide-4', {xPercent: -100})
-    tl.from('.slide-5', {xPercent: 100}, "<")
+    .addLabel('slide-4')
+    .to('.slide-4', {xPercent: -100})
+    .from('.slide-5', {xPercent: 100}, "<")
 
-    tl.to('.slide-5', {xPercent: -100})
-    tl.from('.slide-6', {xPercent: 100}, "<")
+    .addLabel('slide-5')
+    .to('.slide-5', {xPercent: -100})
+    .from('.slide-6', {xPercent: 100}, "<")
 
-    tl.to('.slide-6', {xPercent: -100})
-    tl.from('.slide-7', {xPercent: 100}, "<")
+    .addLabel('slide-6')
+    .to('.slide-6', {xPercent: -100})
+    .from('.slide-7', {xPercent: 100}, "<")
   
     ScrollTrigger.create({
         animation: tl,
@@ -31,6 +40,12 @@
         end: '+=20000',
         scrub: 1,
         pin: true,
+        snap: {
+			snapTo: 'labels',
+			duration: { min: 0.2, max: 3 },
+			delay: 0,
+			// ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
+		}
     })
 
 });
