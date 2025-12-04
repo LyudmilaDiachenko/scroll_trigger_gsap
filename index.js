0bc4e5280be5c91dd90ad61821e2aa02c7e1cacb
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-12-04 21:25:02 +0200

    video slides mvp

--- index.js
+++ index.js
@@ -35,19 +35,44 @@
     .addLabel('slide-6')
     .to('.slide-6', {xPercent: -100})
     .from('.slide-7', {xPercent: 100}, "<")
+
+    .addLabel('finish')
   
     ScrollTrigger.create({
-        animation: tl,
-        trigger: '.main',
-        end: '+=20000',
-        scrub: 1,
-        pin: true,
-        snap: {
-			snapTo: 'labels',
-			duration: { min: 0.2, max: 3 },
-			delay: 0,
-			// ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
-		}
-    })
+      animation: tl,
+      trigger: '.main',
+      end: '+=20000',
+      scrub: 1,
+      pin: true,
+      snap: {
+        snapTo: 'labels',
+        duration: { min: 0.2, max: 3 },
+        delay: 0,
+        // ease: 'power1.inOut' // the ease of the snap animation ("power3" by default)
+      }
+    });
 
+    const popup = document.querySelector('.pop-up');
+    const overlay = document.querySelector('.overlay');
+    const video = popup.querySelector('.pop-up video')
+    const close = document.querySelectorAll('.close, .overlay')
+    document.querySelectorAll('.video').forEach(el => {
+      el?.addEventListener('click', event => {
+        video.src = `./video/${event.target.dataset.name}.webm`
+        gsap.fromTo(overlay, {opacity: 0.1}, {opacity: 0.9});
+        overlay?.classList.add('open')
+        gsap.from(popup, {scale: 0.8, opacity: 0.5});
+        popup?.classList.add('open')
+        video.play()
+        event.target.classList.add('viewed')
+      })
+      close.forEach(el => el.addEventListener('click', _ => {
+        gsap.fromTo(overlay, {opacity: 0.9}, {opacity: 0.1});
+        gsap.to(popup, {scale: 0.8, opacity: 0.5});
+        popup.classList.remove('open')
+        overlay.classList.remove('open')
+        video.pause()
+      }))
+    })
 });
+
